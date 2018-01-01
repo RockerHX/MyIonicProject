b5007ee43cc54b08f7fe6e8146c165c94cbedcb1
RockerHX <rockerhx@gmail.com>
2018-01-01 14:35:40 +0800

    Add[Page]: Alerts
    For show all alerts demo.

---
+++ src/pages/alerts/alerts.ts
@@ -0,0 +1,116 @@
+import { Component } from '@angular/core';
+
+import { 
+  ActionSheetController,
+  AlertController
+ } from 'ionic-angular';
+
+/**
+ * Generated class for the AlertsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-alerts',
+  templateUrl: 'alerts.html',
+})
+export class AlertsPage {
+  
+  constructor(
+    public actionSheetController: ActionSheetController,
+    public alertController: AlertController
+  ) {}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad AlertsPage');
+  }
+
+  presentActionSheet(): void {
+    let actionSheet = this.actionSheetController.create({
+      title: 'Modify your album',
+      buttons: [
+        {
+          text: 'Destructive',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        },{
+          text: 'Archive',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  showAlert(): void {
+    let alert = this.alertController.create({
+      title: 'New Friend!',
+      subTitle: 'Your friend, Obi wan Kenobi, just accepted your friend request!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  showPrompt(): void {
+    let prompt = this.alertController.create({
+      title: 'Login',
+      message: "Enter a name for this new album you're so keen on adding",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  showConfirm(): void {
+    let confirm = this.alertController.create({
+      title: 'Use this lightsaber?',
+      message: 'Do you agree to use this lightsaber to do good across the intergalactic galaxy?',
+      buttons: [
+        {
+          text: 'Disagree',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Agree',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+}
